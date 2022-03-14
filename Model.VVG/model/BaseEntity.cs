d8c3def7147718e7ba0fa1895941f846cadd9180
tehica <tehica97@gmail.com>
2022-03-14 17:48:09 +0100

    Add project files.

---
+++ Model.VVG/model/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Model.VVG.model
+{
+    internal class BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+}
---
+++ MusaProject/Form1.cs
@@ -0,0 +1,10 @@
+namespace MusaProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
