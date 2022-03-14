42162448cc69c15cdb5411b716b6449c24600bfe
tehica <tehica97@gmail.com>
2022-03-14 18:51:47 +0100

    add dbContext and some dependencies

---
+++ REST.API.VVG/Database/WarehouseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace REST.API.VVG.Database
+{
+    public class WarehouseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public WarehouseDbContext()
+        {
+            //AppContext.SetSwitch
+        }
+    }
+}
