f71dd504e0614777ea99e663b51b993b57a9ae97
tehica <tehica97@gmail.com>
2022-04-25 18:52:06 +0200

    new change

--- Console.VVG/Program.cs
+++ Console.VVG/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Console.VVG.Database;
+using Model.VVG.model;
+
+var dbcontext = new WarehouseDbContext();
+
+Company cmp = new Company
+{
+    Id = 4,
+    CompanyName = "Company 4",
+    Address = "Address 4",
+    Contact = "Contact 4"
+};
+dbcontext.Company.Add(cmp);
+
+Company company = dbcontext.Company.Find(4);
+System.Console.WriteLine(company.Id);
+System.Console.WriteLine(company.Address);
