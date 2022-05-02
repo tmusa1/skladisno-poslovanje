46d3ce25b348cb13f8337a84b4f2ec3d1576c8d1
tehica <tehica97@gmail.com>
2022-05-02 18:14:02 +0200

    implement all http methods in Company controller

--- REST.API.VVG/Controllers/CompanyController.cs
+++ REST.API.VVG/Controllers/CompanyController.cs
@@ -19,5 +19,50 @@ namespace REST.API.VVG.Controllers
                 companies = _db.Company.ToList();
             return companies;
         }
+
+        [HttpGet("{id}")]
+        public Company? getById(int id)
+        {
+            Company? company = _db.Company.Find(id);
+            return company;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult delete(int id)
+        {
+            Company? company = _db.Company.Find(id);
+            if (company != null)
+            {
+                _db.Company.Remove(company);
+                _db.SaveChanges();
+                return new OkResult();
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IEnumerable<Company>? update([FromBody] Company company)
+        {
+            List<Company> responseBundle = new List<Company>();
+
+            //if (!validate(company))
+            //    return responseBundle;
+
+            EntityEntry<Company>? storedResource;
+
+            if (_db.Company != null)
+            {
+                if (company.Id < 1)
+                    storedResource = _db.Company.Add(company);
+                else
+                    storedResource = _db.Company.Update(company);
+
+                _db.SaveChanges();
+                responseBundle.Add(storedResource.Entity);
+            }
+
+            return responseBundle;
+
+        }
     }
 }
